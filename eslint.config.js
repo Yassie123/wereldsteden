dd52ca93f9f48e5d2309a80112e89b969394c0f4
Yasmine Stubbe <yasminestubbe@gmail.com>
2025-03-11 18:56:30 +0100

    postcss geinstalleerd

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,22 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-
+import globals from 'globals';
+import pluginJs from '@eslint/js';
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {languageOptions: { globals: globals.browser }},
+  { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
 ];
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: 'eslint:recommended',
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {},
+};
